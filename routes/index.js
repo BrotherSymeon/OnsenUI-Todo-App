91702faed559887ec7369f5e305d5259825665d3
fr.Innocent <brother.symeon@gmail.com>
2019-11-07 13:51:55 -0500

    added deployhook

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/deployhook', function(req, res){
+  var {exec} = require('child_process');
+  exec('git pull; refresh', function(error, stdout, stderr) {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return res.send(500);
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+    res.send(200);
+  });
+});
 
 module.exports = router;
