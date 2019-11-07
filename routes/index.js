9f08e9694fd4fb234e184d31152954033e33fa6a
Glitch (brothersymeon-onsenui-todo-app) <none>
2019-11-07 19:55:44 +0000

    👷👗 Checkpoint ./routes/index.js:7306958/2470

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
-router.get('/deployhook', function(req, res){
-  var {exec} = require('child_process');
-  exec('git pull ; git merge $(git rev-parse --abbrev-ref HEAD) ; refresh', function(error, stdout, stderr) {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return res.sendStatus(500);
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-    res.sendStatus(200);
-  });
+router.get("/deployhook", function(req, res) {
+  console.log(req.query.secret);
+  if (req.query.secret === process.env.SECRET) {
+    console.log(`they match`);
+    var { exec } = require("child_process");
+    exec(
+      "git pull ; git merge $(git rev-parse --abbrev-ref HEAD) ; refresh",
+      function(error, stdout, stderr) {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return res.sendStatus(500);
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        res.sendStatus(200);
+      }
+    );
+  }else{
+    console.log(`they Did NOT match`);
+    return res.sendStatus(500);
+  }
 });
 
 module.exports = router;
