ec52c61291f0c53439fe58f57a9a0efab0e3b9a7
fr.Innocent <brother.symeon@gmail.com>
2019-11-08 14:52:03 -0500

    changed one character

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ function eventsHandler(req, res){
   console.log(`${req.event_type} event recieved`);
     var { exec } = require("child_process");
     exec(
-      "git pull ; git merge $(git rev-parse --abbrev-ref HEAD) ; refresh",
+      "git pull; git merge $(git rev-parse --abbrev-ref HEAD) ; refresh",
       function(error, stdout, stderr) {
         if (error) {
           console.error(`exec error: ${error}`);
